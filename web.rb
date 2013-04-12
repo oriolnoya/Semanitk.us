0b940d6d4fcd08ca458b9a1775030e4bf69a5911
Oriol Noya <uriyeahh@gmail.com>
2013-04-12 19:49:28 +0200

    Sinatra prova routes 1

--- web.rb
+++ web.rb
@@ -3,3 +3,9 @@ require 'sinatra'
 get '/' do
   "Hello, world"
 end
+
+get '/hello/:name' do
+  # matches "GET /hello/foo" and "GET /hello/bar"
+  # params[:name] is 'foo' or 'bar'
+  "Hello #{params[:name]}!"
+end
